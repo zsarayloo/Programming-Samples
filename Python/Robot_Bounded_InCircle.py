8f2ec82ab79df2082dacef88b985469cf791c682
Zahra <zsarayloo@gmail.com>
2024-08-01 01:49:55 -0400

    define function

--- Python/Robot_Bounded_InCircle.py
+++ Python/Robot_Bounded_InCircle.py
@@ -1,7 +1,20 @@
 
 
-def isRobotBounded(Ins):
-    return Ins
+def isRobotBounded(instructions):
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        x, y = 0, 0  
+        dir_index = 0  
+
+        for instruction in instructions:
+            if instruction == 'G':
+                x += directions[dir_index][0]
+                y += directions[dir_index][1]
+            elif instruction == 'L':
+                dir_index = (dir_index - 1) % 4
+            elif instruction == 'R':
+                dir_index = (dir_index + 1) % 4
+
+        return (x == 0 and y == 0) or (dir_index != 0)
 
 
 
