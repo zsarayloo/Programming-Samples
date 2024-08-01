69ff187cd112ffe2e69e0a5e80f6005bb9252d2c
zahra <zsarayloo@gmail.com>
2024-08-01 15:33:19 -0400

    Update Tic-Tac-Toe.py

--- Python/Tic-Tac-Toe.py
+++ Python/Tic-Tac-Toe.py
@@ -1,6 +1,15 @@
+'''
+### Tic-tac-toe is played by two players A and B on a 3 x 3 grid. The rules of Tic-Tac-Toe are:
+    Players take turns placing characters into empty squares ' '.
+    The first player A always places 'X' characters, while the second player B always places 'O' characters.
+    'X' and 'O' characters are always placed into empty squares, never on filled ones.
+    The game ends when there are three of the same (non-empty) character filling any row, column, or diagonal.
+    The game also ends if all squares are non-empty.
+    No more moves can be played if the game is over.
+Given a 2D integer array moves where moves[i] = [rowi, coli] indicates that the ith move will be played on grid[rowi][coli]. return the winner of the game if it exists (A or B). In case the game ends in a draw return "Draw". If there are still movements to play return "Pending".
+'''
 
 import numpy as np
-
 
 def tictactoe(moves):
     board = np.zeros([3,3])
@@ -36,11 +45,7 @@ def tictactoe(moves):
 ###### Test case
 
 M = [[0,0],[1,1],[0,1],[0,2],[1,0],[2,0]]
-
-
 result = tictactoe(M)
-
-
 print ( 'The result of game is :')
 print(result)
 
